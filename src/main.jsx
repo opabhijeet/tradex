17509f0c46a2af4440c58e0e57c38d2ffd48ecea
opabhijeet <abhijeet.2201164ec@iiitbh.ac.in>
2024-12-31 14:21:14 +0530

    add prediction card component

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import PredictionCard from './PredictionCard.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
+    <PredictionCard />
   </StrictMode>,
 )
